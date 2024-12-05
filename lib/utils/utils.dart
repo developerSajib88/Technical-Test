ab4ac6670b98577d8df40ec0b7493f4b1880a369
developerSajib88 <developersajib88@gmail.com>
2024-12-05 09:30:45 +0600

    Initial Commit

---
+++ lib/utils/utils.dart
@@ -0,0 +1,7 @@
+export 'package:technical_test/utils/constants/image_constants.dart';
+export 'package:technical_test/utils/constants/text_constants.dart';
+export 'package:technical_test/utils/constants/ui_constants.dart';
+export 'package:technical_test/utils/styles/color_palates.dart';
+export 'package:technical_test/utils/styles/custom_text_styles.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:google_fonts/google_fonts.dart';
